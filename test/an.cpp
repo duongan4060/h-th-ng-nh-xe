d42ada2ef52c556cf001a4398317b09c8e6c81c7
an nguyen <Nguyenhoangduongan20@gmail.com>
2023-04-07 00:58:13 +0700

    xongnha

--- test/an.cpp
+++ test/an.cpp
@@ -3,10 +3,9 @@
 Servo myservo;
 
 // khai báo biến
-#define trigpin 10
-#define echopin 12
+int trigpin=10;
+int echopin=11;
 int time,distance,pos=0;
-
 // vận tóc của xung phát ra là 340m/s hay là 29.412 microSeconds/cm (106 / (340*100))
 int speed=29.412;
 
@@ -20,27 +19,23 @@ void setup() {
 // hàm để thùng rác mở ra
 void open()
 {
-  for(pos = 0; pos < 130; pos += 1)  
-  {                                     
+  for(pos = 0; pos < 90; pos += 1)  
+  {                                   
     myservo.write(pos);              
-    delay(15);                        
+    delay(15);                       
   } 
 }
-
 // hàm để đóng thùng rác lại 
 void closer()
 {
-  for(pos = 130; pos>=1; pos-=1)     
+  for(pos = 90; pos>=1; pos-=1)     
   {                                
     myservo.write(pos);              
     delay(15);                       
   } 
 }
-
+void loop() {
   // phát xung
-void phatxung(){
-  time=-10;
-  distance=100;
   digitalWrite(trigpin,0);
   delayMicroseconds(2);
   digitalWrite(trigpin,1);
@@ -51,20 +46,13 @@ void phatxung(){
   // tính toán đưa về khoảng cách
   distance=int(time/2/speed);
   Serial.println(distance);
-}
-
-void loop() {
-  phatxung();
-  if(distance<40){
+// điều kiện để hoạt động
+  if (distance<=40)
+  {
     open();
-    while (distance<40)
-    {
-      phatxung();
-      if(distance>=40){
-      closer();
-    }
-    }
-    
-
+    delay(1000);
+    closer();
   }
-}
+    /// xinh chào các bạns
+    ///xin chào các bạn
+} 
